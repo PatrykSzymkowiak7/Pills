28679606a89081acaabb55bed75a7c36ad2dc418
PatrykSzymkowiak7 <87982924+PatrykSzymkowiak7@users.noreply.github.com>
2026-01-15 13:18:45 +0100

    Adding pagination

---
+++ Pills/Models/ViewModels/PillsTaken/HistoryPagedViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pills.Models.ViewModels.PillsTaken
+{
+    public class HistoryPagedViewModel
+    {
+        public List<HistoryDayViewModel> Days { get; set; } = new();
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
